75ff9bce9fac701a707b73cc3c5af553716505d9
Paul Whipp <pwhipp@csumb.edu>
2020-01-04 10:00:26 -0800

    first commit

---
+++ forum/src/test/java/whipp/demo/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package whipp.demo.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
